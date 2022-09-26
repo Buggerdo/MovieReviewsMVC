cbdae5a6b28cfe1f9939763116716c1341b821f7
Troy <wilson.troy.anthony@gmail.com>
2022-09-26 19:09:19 -0400

    Added movie class

---
+++ MovieReviewsMVC/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieReviewsMVC.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+        
+        [StringLength(30, MinimumLength = 3)]
+        public string? Genre { get; set; }
+        
+        [Range(1, 300)]
+        public int? Runtime { get; set; }
+        
+        [StringLength(30)]
+        public string? ParentalRating { get; set; }
+        
+        [StringLength(60, MinimumLength = 3)]
+        public string? Director { get; set; }
+    }
+}
+// Scaffold-DbContext 'Data Source=TROYSPC;Initial Catalog=MovieReviewsMvc;Integrated Security=SSPI;' Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
+// add-migration MovieReviewsMvc
